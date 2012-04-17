a12fbe82711135f53103841fcdf2e9d851cc7263
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-04-17 15:08:22 +0200

    modules/buildenv/manifests/deb.pp - Add pkg-config

--- manifests/deb.pp
+++ manifests/deb.pp
@@ -1,7 +1,7 @@
 class buildenv::deb {
 
   package {
-    ["lintian", "dput", "dpatch", "devscripts", "build-essential", "dh-make", "dpkg-dev", "fakeroot", "cdbs"]:
+    ["lintian", "dput", "dpatch", "devscripts", "build-essential", "dh-make", "dpkg-dev", "fakeroot", "cdbs", "pkg-config"]:
     ensure => present,
   }
 
