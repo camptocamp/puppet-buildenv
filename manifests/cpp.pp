45789760092ce3503b3388dbf92fb679bfaa5cb5
Mickaël Canévet <mickael.canevet@camptocamp.com>
2012-12-05 17:45:28 +0100

    Lint buildenv::cpp

--- manifests/cpp.pp
+++ manifests/cpp.pp
@@ -1,9 +1,11 @@
 class buildenv::cpp inherits buildenv::c {
 
-  package { "g++":
-    name => $operatingsystem ? {
-      /RedHat|Fedora|CentOS/ => "gcc-c++",
-      /Debian|Ubuntu/ => "g++",
-    },
+  $package_name = $::operatingsystem ? {
+    /RedHat|Fedora|CentOS/ => 'gcc-c++',
+    /Debian|Ubuntu/        => 'g++',
+  }
+
+  package { 'g++':
+    name => $package_name,
   }
 }
