54e41829cd49c6cc712481629112acb2db41b0b5
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-03-22 09:53:50 +0100

    Use osfamily

--- manifests/rpm.pp
+++ manifests/rpm.pp
@@ -8,9 +8,9 @@ Setup a build environment for RPM building.
 
 class buildenv::rpm {
 
-  $rpmbuild = $::operatingsystem ? {
-    /Debian|Ubuntu/ => 'rpm',
-    default         => 'rpm-build',
+  $rpmbuild = $::osfamily ? {
+    'Debian' => 'rpm',
+    default  => 'rpm-build',
   }
 
   package { $rpmbuild:
